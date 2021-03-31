bd71c2749785fc15a5e317c62dd40094895ed645
viren-vii <virubhosale112@gmail.com>
2021-03-31 12:44:58 +0530

    PRACTICAL 2

---
+++ p2/index.js
@@ -0,0 +1,8 @@
+window.onload(function(){
+    var nameRef = document.getElementById("name");
+})
+function check(){
+    if(nameRef.value == null){
+        alert("Enter Name Properly!");
+    }
+}
